dc5aa427af7ae759cca90ac58b90f9f2b0a58153
Adeniran J. Olukanni <adesite67@gmail.com>
2022-12-12 19:42:47 +0100

    Fix missing email link in success page.

--- shopiverse_frontend/pages/success.js
+++ shopiverse_frontend/pages/success.js
@@ -24,8 +24,10 @@ export default function Success() {
         <h2>Thank you for your order!</h2>
         <p className='email-msg'>Check your email inbox for the receipt</p>
         <p className='description'>
-          If you have any questions, please email{' '}
-          <a className='email' href='mailto:order@example.com' />
+          If you have any questions, please email us at{' '}
+          <a className='email' href='mailto:support@shopiverse-store.com'>
+            support@shopiverse-store.com
+          </a>
         </p>
         <Link href='/'>
           <button type='button' width='300px' className='btn'>
@@ -36,4 +38,5 @@ export default function Success() {
     </div>
   );
 }
+
 
